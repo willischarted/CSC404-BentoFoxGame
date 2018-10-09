c0f150b8f7184a6e579b223234b567bdf60162ff
fenrirTheGreat <43285217+fenrirTheGreat@users.noreply.github.com>
2018-10-08 23:19:34 -0400

    Franks scene added
    Includes scene and the materials / scripts i used thus far. Have not fully integrated monster ai to be ready to go, but a good start hopefully

---
+++ Waypoint/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using UnityEngine.AI;
+using UnityEngine;
+
+
+public class EnemyMovement : MonoBehaviour {
+
+    Transform traveller;
+    Animator monsterAnim;
+    NavMeshAgent nav;
+    public SphereCollider col;
+    float fieldOfViewAngle = 120f;
+    Vector3 currentTarget;
+    public float speed = 10f;
+    public float maxRotation = 45f;
+    float timer = 0f;
+    Vector3 direction;
+
+
+    private void Awake()
+    {
+        traveller = GameObject.FindGameObjectsWithTag("Traveller")[0].transform;
+        nav = GetComponent<NavMeshAgent>();
+        monsterAnim = GetComponent<Animator>();
+        col = GetComponent<SphereCollider>();
+        currentTarget = transform.position;
+        timer = 0f;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (monsterAnim.GetCurrentAnimatorStateInfo(0).IsName("Stunned"))
+        {
+            return;
+        }
+        else
+        {
+            if (other.gameObject.transform == traveller)
+            {
+                direction = other.transform.position - transform.position;
+                float angle = Vector3.Angle(direction, transform.forward);
+                if (angle < fieldOfViewAngle * 0.5f)
+                {
+                    monsterAnim.SetTrigger("isAlerted");
+                }
+            }
+
+            else if (other.gameObject.transform.tag == "LampLight")
+            {
+                currentTarget = other.gameObject.transform.position;// need to work on this as per traveller script + new methods of movement
+            }
+        }
+        
+    }
+
+
+    void Update() {
+
+
+        if (monsterAnim.GetCurrentAnimatorStateInfo(0).IsName("Stunned"))
+        {
+            Debug.Log("Stunned");
+            nav.SetDestination(transform.position);
+            timer += Time.deltaTime;
+            if (timer > 5)
+            {
+                monsterAnim.SetTrigger("recovered");
+                timer = 0f;
+            }
+        }
+        else if (monsterAnim.GetCurrentAnimatorStateInfo(0).IsName("Chase"))
+        {
+            Debug.Log("Chase");
+            nav.SetDestination(traveller.position);
+        }
+        else if (monsterAnim.GetCurrentAnimatorStateInfo(0).IsName("Alerted"))
+        {
+            Debug.Log("Alerted");
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + transform.up, direction.normalized, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.gameObject.transform == traveller)
+                {
+                    monsterAnim.SetTrigger("travellerSpotted");
+                }
+
+                else
+                {
+                    if (monsterAnim.GetCurrentAnimatorStateInfo(0).IsName("Alerted"))
+                    {
+                        monsterAnim.SetTrigger("isLooking");
+                        currentTarget = traveller.position;
+                    }
+                }
+            }
+        }
+        else if (monsterAnim.GetCurrentAnimatorStateInfo(0).IsName("Investigating"))
+        {
+            Debug.Log("Investigating");
+            nav.SetDestination(currentTarget);
+            if (Vector3.Distance(transform.position, currentTarget) < 1)
+            {
+                monsterAnim.SetTrigger("nothingFound");
+                currentTarget = transform.position;
+                Debug.Log("Reset");
+            }
+        }
+        else
+        {
+            nav.SetDestination(currentTarget);
+        }
+    
+
+    }
+}
---
+++ Waypoint/Assets/Scripts/basicMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class basicMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float sidewaysForce = 5f;
+    public Animator monsterAnim;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(sidewaysForce, 0, 0, ForceMode.VelocityChange);
+        }
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(0, 0, sidewaysForce , ForceMode.VelocityChange);
+        }
+
+        if(Input.GetKey("a"))
+        {
+            rb.AddForce(0, 0, -sidewaysForce, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(-sidewaysForce , 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("p"))
+        {
+            monsterAnim.SetTrigger("isStunned");
+        }
+
+    }
+}
