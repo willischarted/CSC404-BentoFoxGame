24a215887b6dc1178e11082712d2d0d35bc3b4c7
fenrirTheGreat <43285217+fenrirTheGreat@users.noreply.github.com>
2018-10-16 18:47:44 -0400

    monsters fixed tagging and put on scene
    made reduced function monster, inverted colours to see easily, placed monsters on joys scene

--- Waypoint/Assets/Scripts/EnemyMovementReduced.cs
+++ Waypoint/Assets/Scripts/EnemyMovementReduced.cs
@@ -3,7 +3,8 @@ using UnityEngine.AI;
 using UnityEngine;
 
 
-public class EnemyMovement : MonoBehaviour {
+public class EnemyMovementReduced : MonoBehaviour
+{
 
     Transform traveller;
     Animator monsterAnim;
@@ -33,32 +34,9 @@ public class EnemyMovement : MonoBehaviour {
         timer = 0f;
     }
 
-    void OnTriggerStay(Collider other)
+
+    void Update()
     {
-        if (monsterAnim.GetCurrentAnimatorStateInfo(0).IsName("Stunned"))
-        {
-            Debug.Log("inhere");
-            return;
-        }
-        else
-        {
-            if (other.gameObject.transform == traveller)
-            {
-                direction = other.transform.position - transform.position;
-                float angle = Vector3.Angle(direction, transform.forward);
-                if (angle < fieldOfViewAngle * 0.5f)
-                {
-                    monsterAnim.SetTrigger("isAlerted");
-                }
-            }
-
-         
-        }
-        
-    }
-
-
-    void Update() {
 
 
         if (monsterAnim.GetCurrentAnimatorStateInfo(0).IsName("Stunned"))
@@ -72,73 +50,36 @@ public class EnemyMovement : MonoBehaviour {
                 timer = 0f;
             }
         }
-        else if (monsterAnim.GetCurrentAnimatorStateInfo(0).IsName("Chase"))
-        {
-            Debug.Log("Chase");
-            nav.SetDestination(traveller.position);
-            if (Vector3.Distance(roamCenterPoint, traveller.position) <= maxRoamDistance)
-            {
-                monsterAnim.SetTrigger("travellerLost");
-            }
-        }
-        else if (monsterAnim.GetCurrentAnimatorStateInfo(0).IsName("Alerted"))
-        {
-            Debug.Log("Alerted");
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position + transform.up, direction.normalized, out hit, Mathf.Infinity))
-            {
-                if (hit.collider.gameObject.transform == traveller)
-                {
-                    monsterAnim.SetTrigger("travellerSpotted");
-                }
 
-                else
-                {
-                    if (monsterAnim.GetCurrentAnimatorStateInfo(0).IsName("Alerted"))
-                    {
-                        monsterAnim.SetTrigger("isLooking");
-                        currentTarget = traveller.position;
-                    }
-                }
-            }
-        }
-        else if (monsterAnim.GetCurrentAnimatorStateInfo(0).IsName("Investigating"))
-        {
-            Debug.Log("Investigating");
-            nav.SetDestination(currentTarget);
-            if (Vector3.Distance(transform.position, currentTarget) < 1 || Vector3.Distance(roamCenterPoint, currentTarget) <= maxRoamDistance)
-            {
-                monsterAnim.SetTrigger("nothingFound");
-                currentTarget = transform.position;
-                Debug.Log("Reset");
-            }
-        }
         else
         {
 
             if (!moving)
-                { // not currently moving, find new place to move to
+            { // not currently moving, find new place to move to
                 Debug.Log("not moving");
                 moveToLamp();
-                }
-            else if (Vector3.Distance(transform.position,currentTarget) < 3.0f) { //reached destination
+            }
+            else if (Vector3.Distance(transform.position, currentTarget) < 3.0f)
+            { //reached destination
                 moving = false;
                 Debug.Log("in the elseif");
                 nav.SetDestination(transform.position);
-                
-                if (lastVisited == null){
+
+                if (lastVisited == null)
+                {
                     lastVisited = findCurrentLamp();
                 }
             }
         }
     }
-    
-    public void moveToLamp() {
+
+    public void moveToLamp()
+    {
         Debug.Log("in here");
         GameObject[] lamps = GameObject.FindGameObjectsWithTag("LampLight");
         List<GameObject> validLamps = new List<GameObject>();
         foreach (GameObject lamp in lamps)
-        {   
+        {
             if (Vector3.Distance(transform.position, lamp.transform.position) <= MAX_LD && Vector3.Distance(roamCenterPoint, lamp.transform.position) <= maxRoamDistance)
             {
                 if (lamp.transform.GetChild(0).GetComponentInChildren<Light>().intensity == 3)
@@ -172,7 +113,7 @@ public class EnemyMovement : MonoBehaviour {
             }
         }
         lamps = validLamps.ToArray();
-        if (lamps.Length!=0)
+        if (lamps.Length != 0)
         {
             int ran = Random.Range(0, lamps.Length - 1);
             GameObject lamp = lamps[ran];
