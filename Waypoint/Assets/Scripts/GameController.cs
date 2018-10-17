5e002accbabfd62e1b64b0b980e9f7e93a6fd6e8
William <willischarted@gmail.com>
2018-10-17 11:41:34 -0400

    updated end state added gamecontroller tentative

---
+++ Waypoint/Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class GameController : MonoBehaviour {
+
+	public GameObject traveller;
+	private travellerScript tScript;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if (traveller is out og health)
+		//SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+
+		//otherwise winning is handeled in traveleler
+		
+	}
+}
