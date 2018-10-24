78fa0fc1568c3be2488b70ab482cc69b07d0ce88
William <willischarted@gmail.com>
2018-10-24 18:16:36 -0400

    added tag and temp image

--- Waypoint/Assets/Scripts/MiniMapController.cs
+++ Waypoint/Assets/Scripts/MiniMapController.cs
@@ -5,8 +5,31 @@ using UnityEngine;
 public class MiniMapController : MonoBehaviour {
 
 	public Transform player;
+	public Transform trav;
 
 	public Transform playerCamera;
+
+	public Sprite travellerIcon;
+
+	
+	void Start() {
+
+	}
+
+	void Update() {								//temp value to check	
+		//if (Vector3.Distance(player.position, trav.position) > 10f) {	
+			//set the icon to the edge of the screen 
+		//}
+		//get the angle of rotation from the player
+		if (Input.GetKeyDown(KeyCode.L)) {
+			Debug.Log("the angle between the trav and the player is: ");
+			//Debug.Log(Vector3.Angle(player.position, trav.position));
+			float fAngle = Vector3.Cross(player.position.normalized,trav.position.normalized).y;
+			fAngle *= 180.0f;
+			Debug.Log(fAngle);
+
+		}
+	}
 
 
 	
