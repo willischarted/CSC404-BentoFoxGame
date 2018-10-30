8d714e735c719cd7627fd19e5e6d9ba6612939a9
iamyuchy <iamyuchy@gmail.com>
2018-10-30 19:05:09 -0400

    delete collider in level3

---
+++ Waypoint/Assets/nextLevelScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextLevelScript : MonoBehaviour {
+
+    public void nextLevelBtn()
+    {
+        int current = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(current + 1, LoadSceneMode.Single);
+    }
+}
