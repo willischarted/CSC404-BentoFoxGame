5329fc7398ac7a6b2ee36b9d99f01a0cf69a2eb1
Brian Van <b.van@mail.utoronto.ca>
2018-11-03 17:41:46 -0400

    updating various materials

---
+++ Waypoint/Assets/traveller_healthmaterial.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class traveller_healthmaterial : MonoBehaviour {
+    public Material highMat;
+    public Material lowMat;
+    public Light highLight;
+    public Light lowLight;
+    private MeshRenderer my_renderer;
+
+    // Use this for initialization
+    void Start () {
+        my_renderer = GetComponent<MeshRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
