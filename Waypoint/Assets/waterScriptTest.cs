0c66fe4d2a5fab5724272fe034fea486a164c4e4
Brian Van <b.van@mail.utoronto.ca>
2018-11-07 17:51:52 -0500

    setting up level4 art, fixed skybox for some cameras

---
+++ Waypoint/Assets/waterScriptTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class waterScriptTest : MonoBehaviour {
+
+    private Camera cam;
+
+    // Use this for initialization
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        cam.depthTextureMode = DepthTextureMode.Depth;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
