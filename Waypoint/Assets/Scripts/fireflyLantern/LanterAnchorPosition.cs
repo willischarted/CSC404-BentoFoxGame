2b07a689280e830ff73a34fe75140e2bfd167f4c
fenrirTheGreat <43285217+fenrirTheGreat@users.noreply.github.com>
2018-11-11 23:47:14 -0500

    changes to my scene trying to get lantern to work
    for lantern

---
+++ Waypoint/Assets/Scripts/fireflyLantern/LanterAnchorPosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanterAnchorPosition : MonoBehaviour {
+    public Transform leftArm;
+    public Transform rightArm;
+    public Transform handL;
+    public Transform handR;
+    public float x, y, z;
+	// Use this for initialization
+	void Start () {
+        handL.position = leftArm.position+ new Vector3(x, y, z);
+        handL.rotation = leftArm.rotation;
+        handR.position = rightArm.position + new Vector3(x, y, z);
+        handR.rotation = rightArm.rotation;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        handL.position = leftArm.position + new Vector3(x,y,z);
+        handL.rotation = leftArm.rotation ;
+        handR.position = rightArm.position+ new Vector3(x, y, z);
+        handR.rotation = rightArm.rotation;
+    }
+}
