98edcdc8e1b6fe834b0ce7a01c27a5db676f349c
fenrirTheGreat <43285217+fenrirTheGreat@users.noreply.github.com>
2018-11-12 01:08:26 -0500

    made lantern not glitch out
    wrote script needs to edit

--- Waypoint/Assets/Scripts/fireflyLantern/handlePosition.cs
+++ Waypoint/Assets/Scripts/fireflyLantern/handlePosition.cs
@@ -6,13 +6,16 @@ public class handlePosition : MonoBehaviour {
     public Transform handle;
     public Transform handL;
     public Transform handR;
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
         handle.position = Vector3.Lerp(handL.position, handR.transform.position, 0.5f);
+
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    // Update is called once per frame
+    void FixedUpdate () {
         handle.position = Vector3.Lerp(handL.position, handR.transform.position, 0.5f);
+
     }
 }
---
+++ Waypoint/Assets/Scripts/fireflyLantern/shm.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shm : MonoBehaviour {
+
+    public GameObject firefly;
+    public Transform pivot;
+    public float separation;
+    public float speed = 0.5f;
+    private float startAngle;
+    private float endAngle;
+    private float fTimer = 0.0f;
+    private Vector3 v3T = Vector3.zero;
+    public float angleUpLimit;
+    public float angleLowLimit;
+
+    void FixedUpdate()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        startAngle = angleUpLimit - moveVertical * 20;
+        endAngle = angleLowLimit - moveVertical * 20;
+        float f = (Mathf.Sin(fTimer * speed - Mathf.PI / 2.0f) + 1.0f) / 2.0f;
+        v3T.Set(Mathf.Lerp(startAngle, endAngle, f),firefly.transform.rotation.y,0f);
+        pivot.eulerAngles = v3T;
+        fTimer += Time.deltaTime;
+    }
+}
