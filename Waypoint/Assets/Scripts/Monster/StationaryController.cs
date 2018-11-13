ca60e9da69c89d87a254b8d85591194c18d23f0f
CassPJ <31894534+CassPJ@users.noreply.github.com>
2018-11-12 22:21:01 -0500

    Timed stationary monster growls

--- Waypoint/Assets/Scripts/Monster/StationaryController.cs
+++ Waypoint/Assets/Scripts/Monster/StationaryController.cs
@@ -6,8 +6,8 @@ public class StationaryController : MonoBehaviour {
     private int flag;
     public AudioSource awakenSound;
     public AudioSource attackSound;
-    private GameObject traveler;   
-
+    private GameObject traveler;
+    private float soundTimer;
 
     public GameObject monsterGeo;
 
@@ -21,7 +21,7 @@ public class StationaryController : MonoBehaviour {
         awakenSound = transform.Find("Audio Source").transform.GetComponent<AudioSource>();
         awakenSound.enabled = true;
         traveler = GameObject.FindGameObjectWithTag("Traveller");
-
+        soundTimer = 0f;
         anim = monsterGeo.GetComponent<Animator>();
         if (anim == null){
             Debug.Log("Can not find the anim");
@@ -35,15 +35,25 @@ public class StationaryController : MonoBehaviour {
             childMonster1.SetActive(false);
             gameObject.SetActive(false);
         }
-                
+        soundTimer += Time.deltaTime;        
         if (Vector3.Distance(traveler.transform.position, transform.position) < 5)
         {
             Debug.Log("wake up");
-            if (!awakenSound.isPlaying)
+            if (soundTimer < 3)
             {
-                awakenSound.Play();
+                if (!awakenSound.isPlaying)
+                {
+                    awakenSound.Play();
+                }
+            } else
+            {
+                awakenSound.Pause();
             }
-        }    
+        }  
+        if(soundTimer > 5)
+        {
+            soundTimer = 0f;
+        }
     }    
     private void OnTriggerEnter(Collider other)
     {
