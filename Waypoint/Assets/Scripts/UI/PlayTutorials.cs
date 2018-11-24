24f77a5413a46057810415db52317bceb6e95e9d
CassPJ <31894534+CassPJ@users.noreply.github.com>
2018-11-23 22:15:53 -0500

    Add files via upload

---
+++ Waypoint/Assets/Scripts/UI/PlayTutorials.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayTutorials : MonoBehaviour {
+    public GameObject levelStartPopup;
+    private float timer;
+    private bool done;
+	// Use this for initialization
+	void Start () {
+        timer = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (levelStartPopup != null && done != true)
+        {
+            if (timer < 1.5f)
+            {
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                levelStartPopup.SetActive(true);
+                done = true;
+            }
+        }
+	}
+}
