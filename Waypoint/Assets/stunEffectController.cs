7f47f6f597cd86be53460231eece2987776d2d5e
William <willischarted@gmail.com>
2018-11-26 01:51:11 -0500

    updated stun sfx in lv 3.5

--- Waypoint/Assets/stunEffectController.cs
+++ Waypoint/Assets/stunEffectController.cs
@@ -6,16 +6,23 @@ public class stunEffectController : MonoBehaviour {
 
 	ParticleSystem pSystem;
 
+    private AudioSource stunSFX;
+
 	// Use this for initialization
 	void Start () {
 		pSystem = GetComponent<ParticleSystem>();
 		if (pSystem == null)
 			Debug.Log("Could not find pSystem");
+
+        stunSFX = GetComponent<AudioSource>();
+        if (stunSFX == null)
+            Debug.Log("Could not find audio");
 		
 	}
 	
 	public void playStunEffect() {
 		pSystem.Play();
+        stunSFX.Play();
 	}
 
 	public void stopStunEffect() {
