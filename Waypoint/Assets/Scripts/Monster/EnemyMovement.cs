506a8935debeaed6a7ea1ce9562374fdeb4a7dce
William <willischarted@gmail.com>
2018-11-27 16:39:06 -0500

    implemented tun animation

--- Waypoint/Assets/Scripts/Monster/EnemyMovement.cs
+++ Waypoint/Assets/Scripts/Monster/EnemyMovement.cs
@@ -416,7 +416,7 @@ public class EnemyMovement : MonoBehaviour
       //  nav.SetDestination(transform.position);
        // nav.velocity = Vector3.zero;
        // nav.isStopped = true;
-
+        bodyAnim.SetTrigger("isStun");
 
         bodyAnim.SetBool("isMoving", false);
         monsterAnim.SetTrigger("isStunned");
