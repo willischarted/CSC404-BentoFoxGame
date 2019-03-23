6650cce102b8232e39a6bf2156482247f3e7ede2
fenrirTheGreat <43285217+fenrirTheGreat@users.noreply.github.com>
2019-03-22 21:37:11 -0400

    level 3 edit commit
    needs navmesh baked

---
+++ Waypoint/Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+    public Light brokenBulb;
+
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(Flickering());
+	}
+	
+    IEnumerator Flickering()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.value);
+            brokenBulb.enabled = !brokenBulb.enabled;
+        }
+    }
+}
