a2d53bce2d3de303d7180f8fb6848fc0787ad3e3
William <willischarted@gmail.com>
2019-04-15 02:20:47 -0400

    fixed transition for music

--- Waypoint/Assets/Scripts/Music/AudioController.cs
+++ Waypoint/Assets/Scripts/Music/AudioController.cs
@@ -24,27 +24,53 @@ public class AudioController : MonoBehaviour {
     public void checkPlay()
     {
         int index = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log("get acive scene " + index);
 
+        // BECAUSE THIS IS CALLED BEFORE THE NEXT SCENE LOADS WE HAVE
+        // LOOK AT THE CURRENT SCENE TO DECIDE WHAT TO PLAY FOR THE NEXT ONE
+        // We also have to note see level index key below (index unlike level are not
+        // subsequent
+
+        //the first two levels
         if (index <= 2)
         {
-                PlayMusic(ThemeMusic[0]);
+                PlayMusic(ThemeMusic[0]); 
         }
 
-        if (index > 2 && index <= 4)
+        // mon tut and lv 2.5 -> LV 2 COMES BEFORE LV '5' SO WE MUST USE INDEX 2 TO PREP FOR IT
+        if (index ==  2 ||  index == 7)
         {
                 PlayMusic(ThemeMusic[1]);
         }
 
-        if (index > 4 && index <= 6)
+        // trav tut and lv 3
+        if (index == 3 || index == 8)
         {
                 PlayMusic(ThemeMusic[2]);
         }
 
-        if (index > 6 && index <= 9)
+
+        //final three levels
+        if (index == 4 || index ==9 || index == 5)
         {
                 PlayMusic(ThemeMusic[3]);
         }
+       
 
+        /*LEVEL ORDER 
+         * Level - INDEX
+         * START - 0
+         * 1 - 1
+         * 2 - 2
+         * 5 - 7
+         * 2.5 - - 3 
+         * 5.5 - 8
+         * 3 - 4
+         * 7 - 9
+         * 3.5 - 5
+         * 4 - 6
+         * /
+         */
     }
     public void PlayMusic(AudioSource music)
     {
